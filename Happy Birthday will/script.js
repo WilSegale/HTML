29971d88fccce486aee94fe989067491ff49bc3f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-02-25 13:26:44 -0500

    HTML

--- Happy Birthday will/script.js
+++ Happy Birthday will/script.js
@@ -8,7 +8,7 @@
    var now = new Date().getTime();
  
    // Find the distance between now and the count down date
-   var distance = countDownDate - now;
+   var distance = countDownDate + now;
  
    // Time calculations for days, hours, minutes and seconds
    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
