ef4582e92a0471affc8f0647ecd537cdd2c347db
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-03-03 09:32:19 -0500

    Traslate

--- Top10MostEngangerd animes/code.js
+++ Top10MostEngangerd animes/code.js
@@ -17,27 +17,9 @@ function dropdown() {
 	}
   }
 /*<![CDATA[*/
-function initializeGoogleTranslateElement() {
-	new google.translate.TranslateElement({
-		startpageLanguage: "en",
-		layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-	}, "google_translate_element");
+function googleTranslateElementInit() {
+	new google.translate.TranslateElement(
+		{pageLanguage: 'en'},
+		'google_translate_element'
+	);
 }
-
-
-document.onkeydown = function (e) {
-	if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) { //Alt+c, Alt+v will also be disabled sadly.
-	}
-	return false;
-};
-
-function calculate_age(dob) {
-	var diff_ms = Date.now() - dob.getTime();
-	var age_dt = new Date(diff_ms);
-
-	return Math.abs(age_dt.getUTCFullYear() - 2019);
-}
-
-(calculate_age(new Date(2019, 11, 4)));
-
-(calculate_age(new Date(2019, 1, 1)));
