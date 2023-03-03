ef5af716a6dcf169e00703b24bcc9656228cb0d3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-03-03 08:28:07 -0500

    script.js

--- Happy Birthday will/script.js
+++ Happy Birthday will/script.js
@@ -1,31 +1,30 @@
- // Set the date you're counting down to
- var countDownDate = new Date("oct 2, 2023").getTime();
+// Set the date we're counting down to
+var user = prompt("put in a date");
+var countDownDate = new Date(user).getTime();
 
- // Update the count down every 1 second
- var x = setInterval(function() {
- 
-   // Get today's date and time
-   var now = new Date().getTime();
- 
-   // Find the distance between now and the count down date
-   var distance = countDownDate + now;
- 
-   // Time calculations for days, hours, minutes and seconds
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
- 
-   // Display the result in the element with id="timer"
-   document.getElementById("days").innerHTML = days + "d ";
-   document.getElementById("hours").innerHTML = hours + "h ";
-   document.getElementById("minutes").innerHTML = minutes + "m ";
-   document.getElementById("seconds").innerHTML = seconds + "s ";
- 
-   // If the count down is finished, write some text
-   if (distance < 0) {
-     clearInterval(x);
-     document.getElementById("timer").innerHTML = "EXPIRED";
-   }
- }, 1000);
- 
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time.,
+  var now = new Date().getTime();
+	
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+	
+  // Time calculations for days, hours, minutes and seconds
+  var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));//correct
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));// correct 
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 ));//correct
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));//correct
+  var seconds = Math.floor((distance % (1000 * 60)) / (1000));//correct
+	
+  // Output the result in an element with id="demo"
+  if(years<1){
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  }else{
+  document.getElementById("demo").innerHTML = years +"y " +  days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  }
+  
+}, 1000);
