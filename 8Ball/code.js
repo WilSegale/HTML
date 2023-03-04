23815861ad0eb2fbad79d12a5688917ac2daeab7
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-03-04 10:43:27 -0500

    Update code.js

--- 8Ball/code.js
+++ 8Ball/code.js
@@ -21,11 +21,11 @@ var Answer = [
 	"Very doubtful"
   ];
   
-  function output() {
+function output() {
 	var Input = document.getElementById("input") //user input
 	var Output = document.getElementById('output') //the output of the users input
 	var output = Math.floor(Math.random() * Answer.length); //randomly chosses something form the Answers array
-	
+
 	// if the user input help it tells them how to use the programe
 	if (Input.value == "help") {
 		Output.innerText = 'Input something in the input feild and it will output an answer.'
@@ -33,16 +33,11 @@ var Answer = [
 
 	//tells the program if  a number is used
 	else if(Input.value >= 0 || Input.value <= -1){
-		Output.innerText = "No numbers are allowed in the text box only words."
-	}
-
-	//if the input value has nothing in it it say for them to input somthing in it
-	else if (Input.value == "") {
-		Output.innerText = "Please input something in the text box."
+		Output.innerText = "Invalid input"
 	}
 	//output a answer of the users input
 	else {
-		Output.innerText = Answer[output]; //output the answer
+		Output.innerText = Answer[2]; //output the answer
 		Input.value = ""; //clears the user input when they click the 8Ball
 	}
-  }
+}
