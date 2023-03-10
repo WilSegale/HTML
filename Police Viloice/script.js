3cbb900d9552e4e3c8c474b68f276806155c5814
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-03-09 19:19:10 -0500

    HTML

--- Police Viloice/script.js
+++
@@ -1,9 +0,0 @@
-const colors = ['red', 'lightblue']; // Array of background colors to cycle through
-let currentColorIndex = 0; // Index of the current background color
-
-function Police_Violence() {
-  document.body.style.color = colors[currentColorIndex];
-  currentColorIndex = (currentColorIndex + 1) % colors.length; // Cycle through the array of colors
-}
-
-setInterval(Police_Violence, 1000); // Call the function every 5 seconds
