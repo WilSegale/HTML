e0e414d54dabcc9a2c87b6327c608fdd8d817b39
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-03-26 15:30:14 -0400

    login form

--- login form/code.js
+++ login form/code.js
@@ -1,3 +1,12 @@
-function Login(){
-     alert('ehllow world')
-}
+const form = document.getElementById('login-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Send the username and password to the server for authentication
+  // using a fetch or XMLHttpRequest request
+});
+// connect to XMLHTTPREQUEST ?
