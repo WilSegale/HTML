da4beee4109f0c40a90ee2d531ecef6cc34baa70
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-19 08:11:52 -0400

    Update script.js

--- Happy Birthday will/script.js
+++ Happy Birthday will/script.js
@@ -1,11 +1,10 @@
 // Update the count down every 1 second
 var x = setInterval(function () {
 	// Get todays date and time
-	var now = new Date().getTime();
-	var d = new Date();
-	var n = d.getFullYear();
+	var d= new Date();
+	var year = d.getFullYear();
 	// Set the date we're counting down to
-	var countDownDate = new Date("10/2/"+n).getTime();
+	var countDownDate = new Date("10/2/"+year).getTime();
 	var todaysDate = new Date().getTime();
 	var distance = countDownDate - todaysDate+1;
 
