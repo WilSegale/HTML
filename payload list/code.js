c85a2544a703b785daba40c17b2f55855f5a430d
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-05-20 17:01:49 -0400

    update

---
+++ payload list/code.js
@@ -0,0 +1,19 @@
+// code.js
+
+// Function to toggle the dropdown menu
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
