3e89f541f2037f50e1cb1d480052ae9f79f53135
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-09-02 15:00:59 -0400

    Update code.js

--- open_a_folder/code.js
+++ open_a_folder/code.js
@@ -1,18 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
-   const folderInput = document.getElementById('folderInput');
-   const fileList = document.getElementById('fileList');
- 
-   folderInput.addEventListener('change', handleFolderSelection);
- 
-   function handleFolderSelection(event) {
-     const files = Array.from(event.target.files);
-     fileList.innerHTML = '';
- 
-     files.forEach(file => {
-       const listItem = document.createElement('li');
-       listItem.textContent = file.webkitRelativePath;
-       fileList.appendChild(listItem);
-     });
-   }
- });
+  const folderInput = document.getElementById('folderInput');
+  const fileList = document.getElementById('fileList');
+
+  folderInput.addEventListener('change', handleFolderSelection);
+
+  function handleFolderSelection(event) {
+    const files = Array.from(event.target.files);
+    fileList.innerHTML = '';
+
+    files.forEach(file => {
+      const listItem = document.createElement('li');
+      listItem.textContent = file.webkitRelativePath;
+      fileList.appendChild(listItem);
+    });
+  }
+});
+
  
